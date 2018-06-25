6f6574dc53eaf23ffce35e57189cb2839b726a47
Bobby H <rhoffman103@gmail.com>
2018-06-24 22:09:37 -0400

    making js object oriented 1st draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,44 @@
-// variables to hold # wins, losses, guesses left
-var wins = 0
-var losses = 0
-var guessesLeft = 10
-var space = " "
+// Player Object
+var player = {
+    // variables to hold # wins, losses, guesses left, array for guesses
+    wins: 0,
+    losses: 0,
+    guessesLeft: 10,
+    guesses: [],
 
-// arrays for computerKeys and guesses
-var computerKeys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var guesses = [];
-var allGuesses = "";
+    // Player Methods
+    clearGuesses: function () {
+        this.guesses.length = 0;
+    }, 
+    addGuess: function (guess) {
+        this.guesses.push(guess);
+    },
+}
 
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-const ChooseNewKey = function () {
-    chosenKey = computerKeys[Math.floor(Math.random() * computerKeys.length)];
-};
-ChooseNewKey();
-
-// Erase content inside guesses array
-const clearGuesses = function () {
-    guesses.length = 0;
-};
-
-// Print each guess with a comma and space
-const addGuess = function (guess) {
-    guesses.push(guess);
-};
+// computer object
+var computer = {
+    computerKeys: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    ChooseNewKey: function () {
+        chosenKey = this.computerKeys[Math.floor(Math.random() * this.computerKeys.length)];
+    },
+}
 
 const resetGame = function () {
-    guessesLeft = 10;
-    ChooseNewKey();
-    clearGuesses();
-}
+    player.guessesLeft = 10;
+    computer.ChooseNewKey();
+    player.clearGuesses();
+};
+
+computer.ChooseNewKey();
 
 // Creating variables to hold new HTML.
 const updateTracker = function () {
-    document.querySelector("#wins").innerHTML = wins;
-    document.querySelector("#losses").innerHTML = losses;
-    document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-    document.querySelector("#guesses").innerHTML = guesses.join(", ");
+    document.querySelector("#wins").innerHTML = player.wins;
+    document.querySelector("#losses").innerHTML = player.losses;
+    document.querySelector("#guessesLeft").innerHTML = player.guessesLeft;
+    document.querySelector("#guesses").innerHTML = player.guesses.join(", ");
 }
 
 // This function is run whenever the user presses a key.
@@ -46,34 +48,31 @@ document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
 
     // Perform guessCheck
-    var checkDuplicateGuess = guesses.indexOf(userGuess);
+    var checkDuplicateGuess = player.guesses.indexOf(userGuess);
 
     // Check if user input is a letter
-    var isLetter = computerKeys.indexOf(userGuess);
-
-    // if (!userGuess.match(/[a-z]/i)) return;
+    var isLetter = computer.computerKeys.indexOf(userGuess);
     
     // Logic determines outcome of game
     if(isLetter >= 0) {
         if (checkDuplicateGuess >= 0) {
             alert(`Already guessed letter "${userGuess}"!!!`);
-        } else if (userGuess === chosenKey) {
+        } else if (player.userGuess === chosenKey) {
             wins++;
             alert("Winner winner chicken dinner!");
             resetGame();
-        } else if ((userGuess != chosenKey) && (checkDuplicateGuess == -1)) {
-            if (guessesLeft <= 1) {
+        } else if ((player.userGuess != chosenKey) && (checkDuplicateGuess == -1)) {
+            if (player.guessesLeft <= 1) {
                 losses++;
                 alert("You lose! Try again!");
                 resetGame();
             } else {
-                guessesLeft -= 1;
-                addGuess(userGuess);
+                player.guessesLeft -= 1;
+                player.addGuess(userGuess);
             }
         }
     } else {
         alert("Choose a letter");
     }
-
     updateTracker();
 }
